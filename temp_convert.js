5ab8445da5289b0f4b9abfea5dc21d931b52e091
szambre99 <40729881+szambre99@users.noreply.github.com>
2022-08-21 19:15:48 +0530

    Add files via upload

---
+++ temp_convert.js
@@ -0,0 +1,17 @@
+/*let cel=document.getElementById("cel");
+console.log(cel);
+let fah=document.getElementById("fah");
+console.log(fah);
+
+cel.addEventListener('input',function(){
+    let c=this.value;
+    let f=(c*9/5) + 32;
+    fah.value=f;
+    });
+
+fah.addEventListener('input',function(){
+    let f=this.value;
+    f=(f-32)* 5/9;
+    cel.value=f;
+});
+*/
